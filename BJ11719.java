4bd62495ebef8ebfeb40f21bd0ab25c641f89cd9
yumyumsom <95120859+yumyumsom@users.noreply.github.com>
2023-03-27 13:45:34 +0900

    Add files via upload
    BufferedReader 적응용 문제풀기

---
+++ BJ11719.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ11719 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while((s=br.readLine()) != null){
+            System.out.println(s);
+        }
+
+
+    }
+}
