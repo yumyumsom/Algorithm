968c833c52c694cf78ab1a09bbf1bac86f36bb19
yumyumsom <95120859+yumyumsom@users.noreply.github.com>
2023-04-03 15:20:44 +0900

    행렬 덧셈

--- Baekjoon/BJ2738.java
+++ Baekjoon/BJ2738.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 
-public class Main {
+public class BJ2738 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
