7ca799c4177f6f5b1d5bf50a9eacc117516effa4
yumyumsom <95120859+yumyumsom@users.noreply.github.com>
2023-04-04 13:57:06 +0900

    단어 공부

---
+++ Baekjoon/BJ11157.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ11157 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine().toUpperCase();
+
+        int cnt = 0;
+        char maxAlpha = 0;
+        int alpahbet[] = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int idx = c - 65;    //정수형으로 변환해서 인덱스로 사용
+
+            //인덱스는 0부터 25
+            alpahbet[idx]++;
+            if (cnt < alpahbet[idx]) {
+                cnt = alpahbet[idx];
+                maxAlpha = c;
+            }
+        }
+
+        int maxCnt = 0;
+        //최대값(cnt)가 또 존재하는지
+        for (int i = 0; i < alpahbet.length; i++) {
+            if (alpahbet[i] == cnt) {
+                maxCnt++;
+            }
+        }
+
+        if (maxCnt > 1) {
+            System.out.println("?");
+
+        } else {
+            System.out.println(maxAlpha);
+
+        }
+
+    }
+}
