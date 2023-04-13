4b0d36b4b6df8a9e08a261de56823b67aaafc6d5
yumyumsom <95120859+yumyumsom@users.noreply.github.com>
2023-04-13 17:21:28 +0900

    ATM

---
+++ Baekjoon/TOP100/BJ11399.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ11399 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int sum = 0, ans = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            sum+= arr[i];
+            ans+=sum;
+
+        }
+
+        System.out.println(ans);
+
+    }
+
+
+}
