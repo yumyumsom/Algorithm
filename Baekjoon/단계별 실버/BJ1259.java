50f33c64fdd4b5542f265e5d6f1871d56040ca94
yumyumsom <95120859+yumyumsom@users.noreply.github.com>
2023-05-14 16:07:24 +0900

    팰린드롬수

---
+++ Baekjoon/단계별 실버/BJ1259.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ1259 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            String s = br.readLine();
+            if (s.equals("0")) {
+                break;
+            }
+            boolean cnt = true;
+            for (int i = 0; i < s.length() / 2; i++) {
+                if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                    cnt = false;
+                    break;
+                }
+            }
+            if (cnt) {
+                sb.append("yes").append("\n");
+            } else {
+                sb.append("no").append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
