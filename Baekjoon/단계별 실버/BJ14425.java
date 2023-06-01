4f19d8066f17294c8692ff642fbbbc27032d0ace
yumyumsom <95120859+yumyumsom@users.noreply.github.com>
2023-06-01 16:23:55 +0900

    문자열 집합

---
+++ Baekjoon/단계별 실버/BJ14425.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class BJ14425 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashSet<String> set = new HashSet<>();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < M; i++) {
+            if (set.contains(br.readLine())) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
