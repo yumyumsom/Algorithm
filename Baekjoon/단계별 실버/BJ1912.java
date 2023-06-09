5641aef1501018e20f7b7b424181dd81c87ce43e
yumyumsom <95120859+yumyumsom@users.noreply.github.com>
2023-06-09 17:01:19 +0900

    연속합

---
+++ Baekjoon/단계별 실버/BJ1912.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ1912 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < N; i++) {
+            dp[i] = Math.max(arr[i], arr[i] + dp[i - 1]);
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
