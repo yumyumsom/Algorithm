ae3bb5e292ff9c7ed1c7a8e6f4bef17a9612a244
yumyumsom <95120859+yumyumsom@users.noreply.github.com>
2023-12-15 17:36:10 +0900

    배열뒤집기

---
+++ Baekjoon/202312/BJ10811.java
@@ -0,0 +1,37 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ10811 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int arr[] = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = i + 1;
+        }
+        for (int t = 0; t < M; t++) {
+
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken()) - 1;
+            int j = Integer.parseInt(st.nextToken()) - 1;
+
+            while (i < j) {
+                int tmp = arr[i];
+                arr[i++] = arr[j];
+                arr[j--] = tmp;
+
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
